762f6661328d1435ee16c547d8237c07b06d11c9
niboute <noucoulous53@gmail.com>
2023-08-14 11:45:03 +0200

    added missing documentation

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
+"""0-lockboxes.py
+This module contains a method that takes a list of list of integers
+as parameter, and tells if all boxes can be opened
+"""
+
 
 def canUnlockAll(boxes):
+    """Checks if all boxes can be opened
+
+        Args: boxes (List): List of Lists of integers
+
+        Returns:
+            (bool): Tells whether all items in the list can be opened
+    """
     keysDict = {}
 
     for index, box in enumerate(boxes):
