85fcdd45782826f50d32a212ef0596161a4d0b23
niboute <noucoulous53@gmail.com>
2023-08-25 10:05:11 +0200

    fixed typo in function name

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -7,7 +7,7 @@
  * @n: Value for n of new node
  * Return: Pointer to new node, or NULL if error
  */
-listint_t	*add_nodeint_end(listint_t **head, const int n)
+listint_t *insert_node(listint_t **head, int number);
 {
 	listint_t	*node, *ptr;
 
@@ -16,14 +16,14 @@ listint_t	*add_nodeint_end(listint_t **head, const int n)
 	node = malloc(sizeof(*node));
 	if (!node)
 		return (NULL);
-	node->n = n;
+	node->n = number;
 	node->next = NULL;
 	if (!*head)
 		*head = node;
 	else
 	{
 		ptr = *head;
-		while (ptr->next)
+		while (ptr->next && ptr->next->n < number)
 			ptr = ptr->next;
 		ptr->next = node;
 	}
