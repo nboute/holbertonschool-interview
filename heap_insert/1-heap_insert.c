16593e1a3851e044568d5035123e68fc4eae78fb
niboute <noucoulous53@gmail.com>
2023-08-28 15:28:01 +0200

    fixed more compilation errors

--- heap_insert/1-heap_insert.c
+++ heap_insert/1-heap_insert.c
@@ -1,35 +1,6 @@
 #include "binary_trees.h"
 #include <stdlib.h>
 
-heap_t *heap_insert(heap_t **root, int value)
-{
-	heap_t *new_node = binary_tree_node(*root, value);
-	if (!new_node)
-		return (NULL);
-	heap_t *parent = NULL;
-	size_t size = binary_tree_size(*root);
-	size_t index = size + 1;
-
-	if (!new_node)
-		return (NULL);
-	if (!*root)
-	{
-		*root = new_node;
-		return (new_node);
-	}
-	parent = find_binary_node_by_index(*root, index / 2, size);
-	if (index % 2 == 0)
-		parent->left = new_node;
-	else
-		parent->right = new_node;
-	while (new_node->parent && new_node->n > new_node->parent->n)
-	{
-		swap_btree_nodes(new_node, new_node->parent);
-		new_node = new_node->parent;
-	}
-	return (new_node);
-}
-
 void swap_btree_nodes(heap_t *node1, heap_t *node2)
 {
 	int tmp = node1->n;
@@ -60,4 +31,33 @@ binary_tree_t *find_binary_node_by_index(binary_tree_t *tree, size_t index, size
 		return (find_binary_node_by_index(tree->left, index / 2, size));
 	else
 		return (find_binary_node_by_index(tree->right, index / 2, size));
+}
+
+heap_t *heap_insert(heap_t **root, int value)
+{
+	heap_t *new_node = binary_tree_node(*root, value);
+	if (!new_node)
+		return (NULL);
+	heap_t *parent = NULL;
+	size_t size = binary_tree_size(*root);
+	size_t index = size + 1;
+
+	if (!new_node)
+		return (NULL);
+	if (!*root)
+	{
+		*root = new_node;
+		return (new_node);
+	}
+	parent = find_binary_node_by_index(*root, index / 2, size);
+	if (index % 2 == 0)
+		parent->left = new_node;
+	else
+		parent->right = new_node;
+	while (new_node->parent && new_node->n > new_node->parent->n)
+	{
+		swap_btree_nodes(new_node, new_node->parent);
+		new_node = new_node->parent;
+	}
+	return (new_node);
 }
