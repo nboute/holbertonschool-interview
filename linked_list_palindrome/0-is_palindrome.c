a8ad948de8780966949c2c10a1d73eb320a74703
Nicolas Bouté <nicolasboute123@gmail.com>
2023-09-19 13:31:07 +0200

    reverted to previous version and removed malloc

--- linked_list_palindrome/0-is_palindrome.c
+++ linked_list_palindrome/0-is_palindrome.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
  * list_len - returns the number of elements in a linked listint_t list
@@ -24,26 +25,26 @@ size_t list_len(listint_t *ptr)
 */
 int is_palindrome(listint_t **head)
 {
+	int tab[4096];
 	size_t len;
 	size_t i;
-	size_t j;
-	listint_t *current, *end;
+	listint_t *ptr;
 
 	if (!head || !*head)
 		return (1);
 	len = list_len(*head);
-	if (len == 1)
-		return (1);
-	current = *head;
+	ptr = *head;
+	i = 0;
+	while (ptr)
+	{
+		tab[i++] = ptr->n;
+		ptr = ptr->next;
+	}
 	for (i = 0; i < len; i++)
 	{
-		end = current;
-		for (j = 0; j < len - 1; j++)
-			end = end->next;
-		if (current->n != end->n)
+		len--;
+		if (tab[i] != tab[len])
 			return (0);
-		len -= 2;
-		current = current->next;
 	}
 	return (1);
 }
