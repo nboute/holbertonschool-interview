34b21cbd335054378d010f71c21df2227b817f32
Nicolas Bouté <nicolasboute123@gmail.com>
2023-10-04 14:40:50 +0200

    added new assignment - linked_list_cycle

---
+++ linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: pointer to head of list
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+**/
+int check_cycle(listint_t *list)
+{
+	listint_t *ptr1, *ptr2;
+
+	ptr1 = list;
+	ptr2 = list;
+	while (ptr1 != NULL && ptr2 != NULL && ptr2->next != NULL)
+	{
+		ptr1 = ptr1->next;
+		ptr2 = ptr2->next->next;
+		if (ptr1 == ptr2)
+			return (1);
+	}
+	return (0);
+}
