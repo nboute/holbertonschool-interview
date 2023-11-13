ec505d1e965d5bd8d3cb5b9f6fc4d14576d5af19
nboute <noucoulous53@gmail.com>
2023-11-13 14:41:59 +0100

    added main

--- nqueens/0-nqueens.py
+++ nqueens/0-nqueens.py
@@ -45,19 +45,22 @@ def print_nqueens(chessboard: list[int]):
         queens.append([row, chessboard[row]])
     print(queens)
 
+def nqueens():
+    if len(sys.argv) != 2:
+        print('Usage: nqueens N')
+        sys.exit(1)
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print('N must be a number')
+        sys.exit(1)
 
-if len(sys.argv) != 2:
-    print('Usage: nqueens N')
-    sys.exit(1)
-try:
-    n = int(sys.argv[1])
-except ValueError:
-    print('N must be a number')
-    sys.exit(1)
+    if n < 4:
+        print('N must be at least 4')
+        sys.exit(1)
 
-if n < 4:
-    print('N must be at least 4')
-    sys.exit(1)
+    chessboard = [None for row in range(n)]
+    solve_nqueens(n, chessboard)
 
-chessboard = [None for row in range(n)]
-solve_nqueens(n, chessboard)
+if __name__ == '__main__':
+    nqueens()
