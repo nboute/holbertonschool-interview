82a7f045f0447754dec7451290272a3b57f6a23d
nboute <noucoulous53@gmail.com>
2024-02-29 15:35:23 +0100

    fixed issues with algorithm

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -4,31 +4,29 @@ This module contains the function makeChange(coins, total).
 """
 
 
-def makeChange(coins, total):
+def makeChange(coins, total, count=0):
     """Make change for a given total using the fewest number of coins.
     Args:
         coins: a list of integers representing coin denominations
         total: an integer representing the total amount of change to make
     Returns:
         An integer representing the fewest number of coins to make the given
+       
         total. If the total cannot be made with the given coins, return -1.
     """
-    if total < 0:
-        return -1
     if total == 0:
-        return 0
-    if len(coins) == 0:
+        return count
+    if total < 0 or len(coins) == 0:
         return -1
     coins = sorted(coins, reverse=True)
-    i = 0
-    while coins[i] > total:
-        i += 1
-        if i == len(coins):
-            return -1
-    count = total // coins[i]
-    total = total % coins[i]
-    ret = makeChange(coins[i:], total)
-    if ret == -1:
+    
+    remove_coin = makeChange(coins, total - coins[0], count + 1)
+    
+    pop_pile = makeChange(coins[1:], total, count)
+    if remove_coin == -1 and pop_pile == -1:
         return -1
-    else:
-        return count + ret
+    if remove_coin == -1:
+        return pop_pile
+    if pop_pile == -1:
+        return remove_coin
+    return min(remove_coin, pop_pile)
